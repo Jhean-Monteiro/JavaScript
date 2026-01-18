9f2331a473453750bb1f86971d6e6a6fed7ffecd
Jhean Monteiro <jheanmonteiro.2019@gmail.com>
2026-01-18 12:38:53 -0300

    Create teste.js

---
+++ Function/teste.js
@@ -0,0 +1,59 @@
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
+
+const MUITO_GRANDE_MESMO = `// ${"█".repeat(200)}\n`.repeat(12000);
+
+
+const GIGACHAD_BYTES = "tá bom, para de encher o arquivo aí ".repeat(250_000);
